27ff545c4e2b18b35c45f8d82e3a32f49c6e804b
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-17 19:04:22 +0530

    setup the camera

---
+++ HandTrackingMin.py
@@ -0,0 +1,14 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
