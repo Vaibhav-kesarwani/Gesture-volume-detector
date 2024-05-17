fc9b67e06760d5c45140f17c91008e178aeaa634
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-17 19:22:42 +0530

    make the dots to detect the hands fingers and palm and all 21 parts of the hand

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -7,8 +7,17 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)
+
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
