2d3985e1e5e17d433f061c0845dcaaf9989ebf35
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-17 19:59:42 +0530

    make the hand tracer

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -6,8 +6,10 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-
 mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
@@ -16,8 +18,19 @@ while True:
     # print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                cv2.circle(img, (cx, cy), 8, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
