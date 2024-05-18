e559c6ab86458e82298a977a356864ad2e2a0426
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-18 13:30:41 +0530

    added the volume customize

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 import numpy as np
+import HandTracingModule as htm
+import math
 
 ##########################################
 wCam, hCam = 640, 480
@@ -12,14 +14,34 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
+detector = htm.handDetector(detectionCon=0.7)
+
 while True:
     success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        # print(lmList[4], lmList[8])
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        print(length)
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f'FPS: {int(fps)}', (30, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+    cv2.putText(img, f'FPS: {int(fps)}', (30, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
